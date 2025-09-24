e4db14e8bb76e714dda13b83dbeead91a8d5df09
4223843-create <4223843@gmail.com>
2025-09-24 15:28:48 +0300

    A-0.4.0.4

---
+++ app/src/main/java/app/netlify/vermillion_eclair_b71162/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.netlify.vermillion_eclair_b71162.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
